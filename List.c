9a7c571733333643922f42f619fb5ee9b9bef449
Vivek <viveksalve04@gmail.com>
2026-01-03 12:36:55 +0530

    Process & Memory Management

--- List.c
+++ List.c
@@ -72,5 +72,54 @@ region table
 
 argv[0] = myexe
 
+program - static set of instructions
+
+parent child relationship only in os for process 
+
+region
+virtual address space
+physical address space
+
+os commit 4 gb irrespective of ram to each process  -> virtual
+kernel -> 100 mb    
+text segement -> code segment -> static binary instructions
+
+data region -> global variables -> dynamic memory
+
+text region not expandable  -> fixed sized region
+
+hdd -> text , data regions (no stack region)
+ram -> all text,data,stack regions
+
+so file -> shared object -> linux
+dll -> windows
+dilib -> macos
+
+HDD -> inodes 13 member array
+RAM -> page table
+
+every region have their own page tables
+page tables are in a table called region table
+per process region table(pprt)
+process table
+
+
+Algorithms
+1. allocreg()
+2. freereg()
+3. attach
+4. detach
+5. dupreg   -> fork()  , text,data,stack
+6. growreg  -> brk() , sbrk()
+
+memory allocation policies
+1. Demand Paging
+2. Swap partition 
+
+
+process 0 -> swapper process
+process 1 -> init process
+
+
 
 */
---
+++ atexit_1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+void fun()
+{
+    printf("Inside Fun");
+}
+
+void gun()
+{
+    printf("Inside gun");
+}
+
+
+
+int main()
+{
+    printf("Process is created \n");
+
+    atexit(fun);
+    atexit(gun);
+
+    _exit(11);
+}
